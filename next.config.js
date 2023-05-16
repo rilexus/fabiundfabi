79ae8ef3999ba2aaf5a78ddd36cff1bf98959257
rilexus <stanip@hotmail.de>
2023-05-16 10:41:26 +0200

    added nojekyll file

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const repo = 'fabiundfabi'
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
+
 const nextConfig = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   output: 'export',
   distDir: 'docs',
 }
